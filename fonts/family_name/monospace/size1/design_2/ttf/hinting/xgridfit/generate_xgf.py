329eea31bffecb634edeee73c6330a51cdcf9d99
Patrick Thomas <pthomas505@gmail.com>
2024-02-25 14:52:55 -0800

    Updated hinting.

---
+++ fonts/family_name/monospace/size1/design_2/ttf/hinting/xgridfit/generate_xgf.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+env = Environment(
+  loader = FileSystemLoader('.'),
+  trim_blocks = True,
+  lstrip_blocks = True,
+)
+
+
+print(env.get_template('hinting.template').render())
+
+# python3 generate_xgf.py > hinting.xgf
+# xgridfit -i ../../Untitled.ttf -o ../../Untitled-hinted.ttf hinting.xgf
+# ftgrid -d 1280x1024 16 Untitled-hinted.ttf
