3700ac41b588294c4c98082806653dd98c2a8328
Patrick Thomas <pthomas505@gmail.com>
2024-03-02 23:39:10 -0800

    Updated hinting.

--- fonts/family_name/monospace/size1/design_2/ttf/hinting/xgridfit/generate_xgf.py
+++ fonts/family_name/monospace/size1/design_2/ttf/hinting/xgridfit/generate_xgf.py
@@ -12,4 +12,4 @@ print(env.get_template('hinting.template').render())
 
 # python3 generate_xgf.py > hinting.xgf
 # xgridfit -i ../../Untitled.ttf -o ../../Untitled-hinted.ttf hinting.xgf
-# ftgrid -d 1280x1024 16 Untitled-hinted.ttf
+# ftgrid -d 1280x1024 16 ../../Untitled-hinted.ttf
