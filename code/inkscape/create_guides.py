e583f4af4aaf782fb2a6fdb19fb9881573228c7f
Patrick Thomas <pthomas505@gmail.com>
2026-02-20 22:16:37 -0800

    Updated scripts.

--- code/inkscape/create_guides.py
+++ code/inkscape/create_guides.py
@@ -3,35 +3,19 @@ import inkex
 import math
 
 
-def createGuide(position, orientation, parent):
+def create_guide(position, orientation, parent):
   # Creates a sodipodi:guide node
   # (look into inkex's namespaces to find 'sodipodi' value in order to make a "sodipodi:guide" tag)
   inkex.etree.SubElement(parent, '{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}guide', {'position':position, 'orientation':orientation})
 
 
-class Create_Guides(inkex.Effect):
-  def __init__(self):
-    inkex.Effect.__init__(self)
-
-    self.OptionParser.add_option("--scale",
-    action = "store", type = "int",
-    dest = "scale", help = "Scale")
-
-    self.OptionParser.add_option("--x_min",
-    action = "store", type = "int",
-    dest = "x_min", help = "X min");
-
-    self.OptionParser.add_option("--x_max",
-    action = "store", type = "int",
-    dest = "x_max", help = "X max");
-
-    self.OptionParser.add_option("--y_min",
-    action = "store", type = "int",
-    dest = "y_min", help = "Y min");
-
-    self.OptionParser.add_option("--y_max",
-    action = "store", type = "int",
-    dest = "y_max", help = "Y max");
+class CreateGuidesExtension(inkex.EffectExtension):
+  def add_arguments(self, pars):
+    pars.add_argument("--scale", type=int)
+    pars.add_argument("--x_min", type=int)
+    pars.add_argument("--x_max", type=int)
+    pars.add_argument("--y_min", type=int)
+    pars.add_argument("--y_max", type=int)
 
 
   def effect(self):
@@ -45,19 +29,19 @@ class Create_Guides(inkex.Effect):
     sqrt2 = math.sqrt(2.0)
 
 
-    # removes any existing guides
+    # Removes any existing guides.
 
     nv = self.document.xpath('/svg:svg/sodipodi:namedview', namespaces = inkex.NSS)[0]
 
-    # gets all the guides
+    # Gets all of the guides.
     children = self.document.xpath('/svg:svg/sodipodi:namedview/sodipodi:guide', namespaces = inkex.NSS)
 
-    # removes each guide
+    # Removes each guide.
     for element in children:
       nv.remove(element)
 
 
-    # adds the new guides
+    # Adds the new guides.
 
     for x in range(x_min, x_max + 1, scale):
       # vertical guides spaced along the bottom X axis
@@ -65,7 +49,7 @@ class Create_Guides(inkex.Effect):
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '1,0' # guide angle: 90 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
     for y in range(y_min, y_max + 1, scale):
       # horizontal guides spaced along the left Y axis
@@ -73,7 +57,7 @@ class Create_Guides(inkex.Effect):
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '0,1' # guide angle: 0 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
 
     for x in range(x_min, x_max, scale):
@@ -82,13 +66,13 @@ class Create_Guides(inkex.Effect):
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '-1,1' # guide angle: 45 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
       position = (x + scale * (1.0 - 1.0 / sqrt2), y_min) # guide origin
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '-1,1' # guide angle: 45 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
 
       # 135 degree guides spaced along the bottom X axis
@@ -96,13 +80,13 @@ class Create_Guides(inkex.Effect):
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '1,1' # guide angle: 135 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
       position = (x + scale * (1.0 - 1.0 / sqrt2), y_min) # guide origin
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '1,1' # guide angle: 135 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
 
     for y in range(y_min, y_max, scale):
@@ -111,13 +95,13 @@ class Create_Guides(inkex.Effect):
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '-1,1' # guide angle: 45 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
       position = (x_min, y + scale * (1.0 - 1.0 / sqrt2)) # guide origin
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '-1,1' # guide angle: 45 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
 
       # 135 degree guides spaced along the right Y axis
@@ -125,15 +109,14 @@ class Create_Guides(inkex.Effect):
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '1,1' # guide angle: 135 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
       position = (x_max, y + scale * (1.0 - 1.0 / sqrt2)) # guide origin
       position = (round(position[0]), round(position[1])) # round guide origin
       position_str = str(position[0]) + ',' + str(position[1])
       orientation_str = '1,1' # guide angle: 135 degrees
-      createGuide(position_str, orientation_str, nv)
+      create_guide(position_str, orientation_str, nv)
 
 
 if __name__ == '__main__':
-  effect = Create_Guides()
-  effect.affect()
+  CreateGuidesExtension().run()
